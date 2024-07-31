a6174fe55129f0b79201b5e52d2cf9cf4b8a3cd3
Patricia Segantine <pnsegantine@gmail.com>
2024-07-31 10:01:27 +0100

    feat: add cards at home and json server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,65 @@
-export default function Home() {
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Badge } from "@/components/ui/badge";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+
+interface Recipe {
+  title: string,
+  image: string,
+  time: number,
+  description: string,
+  vegan: boolean,
+  id: string
+}
+
+async function getRecipes(): Promise<Recipe[]> {
+  const result = await fetch('http://localhost:5000/recipes')
+  
+  await new Promise((require) => setTimeout(require, 3000))
+  
+  return result.json()
+}
+
+export default async function Home() {
+  const recipes = await getRecipes()
+  
   return (
-   <main>
-     Home
-   </main>
-  );
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes?.map(recipe => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
+              
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+            
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            
+            <CardFooter className="flex justify-between">
+              <Button
+                variant="default"
+                size="sm"
+              >View Recipe</Button>
+              {
+                recipe.vegan && <Badge variant="secondary">Vegan!</Badge>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  )
 }
